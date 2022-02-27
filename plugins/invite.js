3057f4558935818719932f4105fb08526786ee75
Dz <95111600+Dafatrizaa@users.noreply.github.com>
2022-02-27 17:17:38 +0700

    Delete plugins directory

--- plugins/invite.js
+++
@@ -1,7 +0,0 @@
-let handler = m => m.reply(`Gunakan !join untuk memasukan Bot`)
-
-handler.customPrefix = /^Undangan/i
-handler.command = new RegExp
-handler.premium = true
-
-module.exports = handler
